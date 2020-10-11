50dd1a64db6d19584e0b9e37b3bb90fc2179e737
Suguivy <adrisolgo7373@gmail.com>
2020-10-11 14:59:21 +0200

    improved rom loading and created Rom.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,7 @@
-import qualified Data.ByteString as B
+module Main where
+
+import Rom
+
 import System.Environment
 import Numeric (showHex)
 import GHC.Word (Word8, Word16)
@@ -6,13 +9,8 @@ import Data.Bits
 
 main :: IO ()
 main = do
-  --file <- head <$> getArgs
   let file = "roms/pong.ch8"
-  contents <- w8Tow16List . B.unpack <$> B.readFile file
-  print $ map (`showHex` "") contents
-  where w8Tow16List :: [Word8] -> [Word16]
-        w8Tow16List []       = []
-        w8Tow16List (x:[])   = [shift (w8Tow16 x) 8]
-        w8Tow16List (x:y:xs) = let a = w8Tow16 x
-                                   b = w8Tow16 y in (shift a 8 .|. b) : w8Tow16List xs
-        w8Tow16 = fromIntegral :: Word8 -> Word16
+  print =<< stringRom <$> loadRom file
+
+stringRom :: Rom -> [String]
+stringRom = map (`showHex` "")
