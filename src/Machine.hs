6f990aa9831594276fc2379515c10eb68349109e
Ivy <adrisolgo7373@gmail.com>
2020-10-17 16:19:37 +0200

    changed Rom type and some Machine.hs modifications

--- src/Machine.hs
+++ src/Machine.hs
@@ -1,6 +1,9 @@
 module Machine where
 
+import Rom
+
 import qualified Data.Vector.Storable as V
+import Data.Digits
 import SDL.Vect
 import GHC.Word
 
@@ -10,7 +13,7 @@ data Machine = Machine
   { activePixels :: V.Vector (V2 Int)
   , memory       :: V.Vector Word8
   , registers    :: V.Vector Word8
-  , address      :: Word16
+  , pc           :: Word16
   , stack        :: [Word16]
   , delay        :: Int
   , sound        :: Int
@@ -19,13 +22,20 @@ data Machine = Machine
 screenSize :: V2 Int
 screenSize = V2 64 32
 
-newMachine :: Machine
-newMachine = Machine
+newMachine :: Rom -> Machine
+newMachine r = Machine
   { activePixels = V.fromList $ map (V2 1) [1..10]
-  , memory = V.replicate 0x1000 undefined
+  , memory    = V.fromList r
   , registers = V.replicate 16 undefined
-  , address = undefined
-  , stack = undefined
-  , delay = undefined
-  , sound = undefined
+  , pc        = 0x0200
+  , stack     = undefined
+  , delay     = undefined
+  , sound     = undefined
   }
+
+runInstruction :: Machine -> Instruction -> Machine
+runInstruction m 0x00e0 = error "0x00e0"
+runInstruction m 0x00ee = error "0x00ee"
+runInstruction m i = case digits 16 i of
+  [1,_,_,_] -> error "Jump to address nnn"
+  _         -> error "Unimplemented instruction"
--- src/Rom.hs
+++ src/Rom.hs
@@ -1,16 +1,9 @@
 module Rom where
 
 import qualified Data.ByteString as B
-import GHC.Word (Word8, Word16)
-import Data.List.Split
-import Data.Bits
+import GHC.Word (Word8)
 
-type Rom = [Word16]
+type Rom = [Word8]
 
-loadRom :: FilePath -> IO [Word16]
-loadRom p = do
-  toW16 . B.unpack <$> B.readFile p
-  where toW16 :: [Word8] -> [Word16]
-        toW16 = map (\(x:y:[]) -> let a = w x
-                                      b = w y in a `shift` 8 .|. b) . chunksOf 2
-        w = fromIntegral :: Word8 -> Word16
+loadRom :: FilePath -> IO [Word8]
+loadRom p = B.unpack <$> B.readFile p
