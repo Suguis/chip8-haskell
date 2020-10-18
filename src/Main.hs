21dfd13888eab377ff90d831333a2e03dbbf1712
Ivy <adrisolgo7373@gmail.com>
2020-10-18 20:21:01 +0200

    moved mainLoop from Render.hs to Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,10 @@ import Dbg
 import Machine
 import Render
 
+import Control.Monad (unless)
+import Numeric (showHex)
+import SDL
+
 main :: IO ()
 main = do
   let file = "roms/pong.ch8"
@@ -12,3 +16,21 @@ main = do
   m <- newMachine <$> loadRom file
   r <- mainRenderer
   mainLoop r m
+
+mainLoop :: Renderer -> Machine -> IO ()
+mainLoop r m = do
+  es <- pollEvents
+  let eventIsQPress e =
+        case eventPayload e of
+          KeyboardEvent keyboardEvent ->
+            keyboardEventKeyMotion keyboardEvent == Pressed &&
+            keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
+          _ -> False
+      qPressed = any eventIsQPress es
+  rendererDrawColor r $= V4 255 255 255 255
+  drawPoints r $ activePixelsSDL m
+  present r
+  print $ showHex (fetchInstruction m) ""
+  let nm = nextCycle m
+
+  unless qPressed (mainLoop r nm)
--- src/Render.hs
+++ src/Render.hs
@@ -6,27 +6,7 @@ import Machine
 import qualified Data.Vector.Storable as V
 import Foreign.C.Types
 import SDL
-import Control.Monad (unless)
 import Control.Lens
-import Numeric (showHex)
-
-mainLoop :: Renderer -> Machine -> IO ()
-mainLoop r m = do
-  es <- pollEvents
-  let eventIsQPress e =
-        case eventPayload e of
-          KeyboardEvent keyboardEvent ->
-            keyboardEventKeyMotion keyboardEvent == Pressed &&
-            keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
-          _ -> False
-      qPressed = any eventIsQPress es
-  rendererDrawColor r $= V4 255 255 255 255
-  drawPoints r $ activePixelsSDL m
-  present r
-  print $ showHex (fetchInstruction m) ""
-  let nm = nextCycle m
-
-  unless qPressed (mainLoop r nm)
 
 activePixelsSDL :: Machine -> V.Vector (Point V2 CInt)
 activePixelsSDL = V.map (\(V2 x y) -> P $ V2 (fromIntegral x) (fromIntegral y)) . (^. activePixels)
